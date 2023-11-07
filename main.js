7b8106c19dfec2466d27f4eb4ca8d8bc6b5a3720
drHyperion451 <148370239+drHyperion451@users.noreply.github.com>
2023-11-07 16:24:09 +0000

    'Filters updated since' class js logic
    Added API url

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
+
 const UBLOCK_LIST = 
 "https://raw.githubusercontent.com/stephenhawk8054/misc/main/yt-fix.txt"
+const UBLOCK_API_URL = "https://api.github.com/repos/stephenhawk8054/misc/commits?path=yt-fix.txt&page=1&per_page=1";
 const YT_LIST = "https://pastefy.app/G1Txv5su/raw"
+
+
 
 let ISDARKMODE = 
 window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -21,7 +25,7 @@ async function getLastCodeUblock(){
 }
 
 async function getLastYoutubeCode(){
-    let data = await fetchData(YT_LIST)
+    let data = await fetchData(YT_LIST);
 
     // Returns what is the latest youtube code
     let urls = data.trim().split('\n');
@@ -139,6 +143,62 @@ async function areCodesEqual(forceEquality = false, forceOption = '') {
     })
 }
 
+async function getUpdateDate(url){
+    // Gets last commit date from Github API.
+    let data = await fetchData(url);
+
+    let data_json = JSON.parse(data);
+    let o_date = data_json[0].commit.author.date;
+    return o_date
+}
+
+function pastTenseTime(dateiso){
+    // Converts any ISO8601 to date in past tense (an hour ago...)
+    let date = new Date(dateiso);
+    let now = new Date();
+    let secondsPast = (now.getTime() -  date.getTime()) / 1000;
+    let output_string = ""
+
+    // Is this the fastest method? 
+    // I'm skeptic, but...https://stackoverflow.com/questions/6665997/switch-statement-for-greater-than-less-than
+
+    if (secondsPast < 60){ // one minute
+        output_string = parseInt(secondsPast) + ' seconds ago.'
+    } else
+    if (secondsPast < 3600){ // one hour
+        output_string = parseInt(Math.round(secondsPast/60)) + ' minutes ago.'
+    } else
+    if (secondsPast <86400){ // one day
+        output_string = parseInt(Math.round(secondsPast/3600)) + ' hours ago.'
+    } else
+    if (secondsPast < 2419200){ // one month
+        let days = Math.round(secondsPast/86400)
+        if (days == 1) {
+            output_string = 'A day ago.'
+        } else {
+        output_string = parseInt(days) + ' days ago'
+        }
+    } else { // more than a month. Display date as full.
+        const day = date.getDate();
+        const month = date.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ","");
+        const year = date.getFullYear() == now.getFullYear() ? "" :  " "+date.getFullYear();
+        output_string = day + " " + month + year;
+    }
+    return output_string
+}
+async function changeUpdatedDateHTML(){
+    // changes any element with class 'filter-update-since'
+    let dateiso = await getUpdateDate(UBLOCK_API_URL);
+    let past_date_str = pastTenseTime(dateiso);
+
+    let filter_update_since = document.getElementsByClassName('filter-update-since');
+    for (let i = 0; i < filter_update_since.length; i++) {
+        const element = filter_update_since[i];
+        element.innerHTML = past_date_str
+        // adds hover info.
+        element.title = new Date(dateiso).toLocaleString();
+    }
+}
 function newTabAnchors(classname){
     // Automatically sets all anchors with an speciffic class to be open as
     // newtab
@@ -160,6 +220,7 @@ window.addEventListener("load", (event) => {
                           // 'no': It will force the page to always say no
                           // default: Disables debug option.
     );
+    changeUpdatedDateHTML();
 
   });
   
