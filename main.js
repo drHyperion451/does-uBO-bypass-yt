253bb2171e1c06f993bfc4c0add899b5abbcb0e0
drHyperion451 <doctorhyperion451@gmail.com>
2023-11-10 20:28:01 +0000

    The button hides if it has pased less than 10 mins

--- main.js
+++ main.js
@@ -221,17 +221,22 @@ async function quickFilterButtonChange(dateiso){
     //// > 6h -> Full auto.
     let url_auto = 'https://ublockorigin.github.io/uAssets/update-lists.html?listkeys=ublock-quick-fixes';
     let url_manual = url_auto + '&manual=1';
-    let btn_element = document.querySelector('#update-quick-filters > button');
+    let div_btn_element = document.querySelector('#update-quick-filters')
+    let btn_element = document.querySelector('#update-quick-filters button');
     // Compares current time with updated time
     let date = new Date(dateiso);
     let now = new Date();
     let secondsPast = (now.getTime() -  date.getTime()) / 1000;
-    // Main logic:
-    if (secondsPast < 3600*6){
+    // Main logic. Looks illegal to see concat if-else but this is the js way instead of switch.:
+    if (secondsPast < 60*10){ // 10 min
+        div_btn_element.setAttribute('style', 'display:none;');
+    }
+    if (secondsPast < 3600*6){ // 6h
         btn_element.setAttribute('onclick', `window.location='${url_manual}';`);
     } else {
         btn_element.setAttribute('onclick', `window.location='${url_auto}';`);
     };
+
 }
 
 function newTabAnchors(classname){
