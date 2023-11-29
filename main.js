8b86293b2b7825f40411c7d2e9664e8f0b1b70c2
drHyperion451 <doctorhyperion451@gmail.com>
2023-11-29 18:06:46 +0000

    Complete overhaul + fixes 40

--- main.js
+++ main.js
@@ -10,13 +10,48 @@ const UBLOCK_LIST = "https://raw.githubusercontent.com/stephenhawk8054/misc/main
 const UBLOCK_API_URL = "https://api.github.com/repos/stephenhawk8054/misc/commits?path=yt-fix.txt&page=1&per_page=1";
 const YT_LIST = "https://pastefy.app/G1Txv5su/raw"
 const DEPLOY_FORCE = "https://raw.githubusercontent.com/drHyperion451/DUBY-config/main/forceEquality"
-
+const TEST_FAKEGET = false; 
 
 async function fetchData(codeList){
     //// Fetches the uBlock list file
     // TODO: Remove this and the one in are_codes_equal.mjs
     let response = await fetch(codeList);
     return await response.text();
+}
+async function getLastCodeUblock(url_ublock){
+    let data;
+    if (TEST_FAKEGET == true){
+        data = 'TESTING'
+    } else {
+    data = await fetchData(url_ublock)};
+    // Returns the latest uBlock code
+    let codes = data.trim().split('\n');
+    let lastCode = codes[codes.length - 1];
+    return lastCode;
+}
+
+async function getLastYoutubeCode(yt_url){
+    let data;
+    if (TEST_FAKEGET == true){
+        data = 'a/b/c/d/e/TESTING/f/ - 2023-11-27 05:27+00:00'
+    } else{
+    data = await fetchData(yt_url)};
+
+    // Returns the latest YouTube code
+    let urls = data.trim().split('\n'); 
+    let lastUrl = urls[urls.length - 1];
+    
+    // Example: https://www.youtube.com/s/desktop/bd3558ba/ - 2023-11-27 05:27+00:00
+    // Date is in ISO8601 format
+    let infoParts = lastUrl.split(' - ');
+
+    // Code id part
+    let urlParts = infoParts[0].split('/');
+    let lastCode = urlParts[5];
+
+    // Date
+    let dateYoutubeId = new Date(infoParts[1]);
+    return [lastCode, dateYoutubeId];
 }
 
 async function getUpdateDate(url){
@@ -30,7 +65,7 @@ async function getUpdateDate(url){
 
 async function fetchDateAndUpdate({url_api} = {}){
     let dateiso = await getUpdateDate(url_api);
-    await changeUpdatedDateHTML(dateiso);
+    //await changeUpdatedDateHTML(dateiso);
     await quickFilterButtonChange(dateiso);
 
 }
@@ -45,23 +80,37 @@ function newTabAnchors(classname){
     };
 }
 // Main exec:
+async function main(){
+    newTabAnchors('new-tab');
+    try {
+        const [ublockResult, ytResult] = await Promise.all([
+            getLastCodeUblock(UBLOCK_LIST),
+            getLastYoutubeCode(YT_LIST)
+        ]);
+
+        const ID_UBLOCK = ublockResult;
+        const [ID_YT, DATE_YT] = ytResult;
+
+        areCodesEqual({
+            id_ublock: ID_UBLOCK,
+            id_yt: ID_YT,
+            force_deploy_url: DEPLOY_FORCE,
+            forceEquality: 0,
+            forceOption: '',
+        });
+
+        fetchDateAndUpdate({
+            url_api: UBLOCK_API_URL
+        });
+
+        changeUpdatedDateHTML(DATE_YT)
+    } catch (error) {
+        console.error('Error fetching IDs:', error);
+    }
+}
 
 window.addEventListener("load", (event) => {
-    newTabAnchors('new-tab');
-    areCodesEqual({
-        url_ublock: UBLOCK_LIST,
-        yt_url: YT_LIST,
-        force_deploy_url: DEPLOY_FORCE,
-        forceEquality: 0, // false: Website will work as normal
-                        // true: It will force uBlock to have the same id as YT
-        forceOption:'', // 'yes': It will force the page to always say yes
-                          // 'no': It will force the page to always say no
-                          // default: Disables debug option.
-    });
-    fetchDateAndUpdate({
-        url_api: UBLOCK_API_URL
-    });
-
+    main();
   });
   
 
--- modules/are_codes_equal.mjs
+++ modules/are_codes_equal.mjs
@@ -5,7 +5,7 @@ Makes any necessary changes under the html
 */
 
 // Avoid fetching the real API while testing. SHOULD BE ALWAYS DISABLED
-const TEST_FAKEGET = true; 
+
 
 async function fetchData(codeList){
     //// Fetches the list file
@@ -13,41 +13,6 @@ async function fetchData(codeList){
     return await response.text();
 }
 
-async function getLastCodeUblock(url_ublock){
-    let data;
-    if (TEST_FAKEGET == true){
-        data = 'TESTING'
-    } else {
-    data = await fetchData(url_ublock)};
-    // Returns the latest uBlock code
-    let codes = data.trim().split('\n');
-    let lastCode = codes[codes.length - 1];
-    return lastCode;
-}
-
-async function getLastYoutubeCode(yt_url){
-    let data;
-    if (TEST_FAKEGET == true){
-        data = 'a/b/c/d/e/TESTING/f/ - 2023-11-27 05:27+00:00'
-    } else{
-    data = await fetchData(yt_url)};
-
-    // Returns the latest YouTube code
-    let urls = data.trim().split('\n'); 
-    let lastUrl = urls[urls.length - 1];
-    
-    // Example: https://www.youtube.com/s/desktop/bd3558ba/ - 2023-11-27 05:27+00:00
-    // Date is in ISO8601 format
-    let infoParts = lastUrl.split(' - ');
-
-    // Code id part
-    let urlParts = infoParts[0].split('/');
-    let lastCode = urlParts[5];
-
-    // Date
-    let dateYoutubeId = new Date(infoParts[1]);
-    return [lastCode, dateYoutubeId];
-}
 async function checkDeployForceEquality(force_deploy_url){
     let data = await fetchData(force_deploy_url)
 
@@ -106,49 +71,46 @@ function changeBgColor(colorOption){
 }
 
 
-async function logCodes(forceEquality = false, url_ublock, yt_url, force_deploy_url) {
+async function logCodes(forceEquality = false, id_ublock, id_yt, force_deploy_url) {
     // Displays each log codes to any element with 'ublock-code' id.
     // Idea: Make this change to all elements that share a same class? Maybe
     /// this could be interesting to make dynamic changes after
 
-    let [youtube, dateYT] = await getLastYoutubeCode(yt_url);
-    console.log("Youtube latest code: ", youtube);
-    console.log("Youtube latest update time: ", dateYT)
-    document.getElementById("youtube-code").innerHTML = youtube
+    console.log("Youtube latest code: ", id_yt);
+    document.getElementById("youtube-code").innerHTML = id_yt
 
     var deployForceEquality = await checkDeployForceEquality(force_deploy_url);
     console.log("deployForceEquality: ", deployForceEquality);
     
-    var ublock = await getLastCodeUblock(url_ublock);
     // Even with toggleOption is enabled it will display the fetched uBlock code
-    console.log("Ublock fetched latest code: ", ublock); 
+    console.log("Ublock fetched latest code: ", id_ublock); 
     if (forceEquality || deployForceEquality){
-        var ublock = youtube
+        var id_ublock = id_yt
         console.warn("forceEquality is enabled for quick updates. Check the ublock code for changes")
     };
-    document.getElementById("ublock-code").innerHTML = ublock
+    document.getElementById("ublock-code").innerHTML = id_ublock
     
-    return {ublock, youtube}
+    return {id_ublock, id_yt}
 }
 
-async function areCodesEqual({url_ublock, yt_url, force_deploy_url, forceEquality = false, forceOption = ''} = {}) {
+async function areCodesEqual({id_ublock, id_yt, force_deploy_url, forceEquality = false, forceOption = ''} = {}) {
 
     // Compares both codes
-    logCodes(forceEquality, url_ublock, yt_url, force_deploy_url).then(codes => {
+    logCodes(forceEquality, id_ublock, id_yt, force_deploy_url).then(codes => {
         switch (forceOption) {
             // Just for debug options
             case ('yes' || 'no'):
                 console.warn("Debug is enabled!. Please report this message in the Github Repo.")
             case 'yes':
-                codes.ublock = codes.youtube = 1
+                codes.id_ublock = codes.id_yt = 1
                 break;
             case 'no':
-                codes.ublock = 0; codes.youtube = 1
+                codes.id_ublock = 0; codes.id_yt = 1
             default:
                 break;
         }
         // All the changes listed below.
-        if (codes.ublock == codes.youtube) {
+        if (codes.id_ublock == codes.id_yt) {
             // It means uBlock is updated with the last YouTube script.
             document.getElementById('main-answer').innerHTML = "YES";
             changeBgColor('yes');
